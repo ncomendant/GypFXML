78e9bc2473daac01dcd06f22c1009c3fe964e9b2
ncomendant <nicholas.comendant@gmail.com>
2018-12-06 10:44:38 -0800

    Fixed indentation

--- src/gypfxml/model/Inventory.java
+++ src/gypfxml/model/Inventory.java
@@ -7,56 +7,55 @@ public class Inventory {
 	private ArrayList<Part> allParts;
 	
 	public Inventory() {
-		products = new ArrayList<>();
-		allParts = new ArrayList<>();
+            products = new ArrayList<>();
+            allParts = new ArrayList<>();
 	}
 	
 	public void addProduct(Product product) {
-		products.add(product);
+            products.add(product);
 	}
 	
 	public boolean removeProduct(int productID) {
-		Product product = lookupProduct(productID);
-		if (product != null) {
-			products.remove(product);
-			return true;
-		} else {
-			return false;
-		}
+            Product product = lookupProduct(productID);
+            if (product != null) {
+                    products.remove(product);
+                    return true;
+            } else {
+                    return false;
+            }
 	}
 	
 	public Product lookupProduct(int productID) {
-		for (Product product : products) {
-			if (productID == product.getProductId()) {
-				return product;
-			}
-		}
-		return null;
+            for (Product product : products) {
+                    if (productID == product.getProductId()) {
+                            return product;
+                    }
+            }
+            return null;
 	}
 	
 	public void updateProduct(int productID) {
-		
+	    //Defined solely to adhere to UML specifications
 	}
 	
 	public void addPart(Part part) {
-		allParts.add(part);
+            allParts.add(part);
 	}
 	
 	public boolean deletePart(Part part) {
-		return allParts.remove(part);
+            return allParts.remove(part);
 	}
 	
 	public Part lookupPart(int partID) {
-		for (Part part : allParts) {
-			if (part.getPartID() == partID) {
-				return part;
-			}
-		}
-		return null;
+            for (Part part : allParts) {
+                    if (part.getPartID() == partID) {
+                            return part;
+                    }
+            }
+            return null;
 	}
 	
 	public void updatePart(int partID) {
-		
+            //Defined solely to adhere to UML specifications
 	}
-	
 }
--- src/gypfxml/model/Part.java
+++ src/gypfxml/model/Part.java
@@ -9,50 +9,50 @@ public abstract class Part {
 	private int max;
 	
 	public void setName(String name) {
-		this.name = name;
+            this.name = name;
 	}
 	
 	public String getName() {
-		return name;
+            return name;
 	}
 	
 	public void setPrice(double price) {
-		this.price = price;
+            this.price = price;
 	}
 	
 	public double getPrice() {
-		return price;
+            return price;
 	}
 	
 	public void setInStock(int inStock) {
-		this.inStock = inStock;
+            this.inStock = inStock;
 	}
 	
 	public int getInStock() {
-		return inStock;
+            return inStock;
 	}
 	
 	public void setMin(int min) {
-		this.min = min;
+            this.min = min;
 	}
 	
 	public int getMin() {
-		return min;
+            return min;
 	}
 	
 	public void setMax(int max) {
-		this.max = max;
+            this.max = max;
 	}
 	
 	public int getMax() {
-		return max;
+            return max;
 	}
 	
 	public void setPartID(int partID) {
-		this.partID = partID;
+            this.partID = partID;
 	}
 	
 	public int getPartID() {
-		return partID;
+            return partID;
 	}
 }
