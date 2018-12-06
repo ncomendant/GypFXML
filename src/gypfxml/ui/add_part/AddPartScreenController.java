f7db7338e0afe8555afb0e754f71ce4a25bdd090
ncomendant <nicholas.comendant@gmail.com>
2018-12-06 02:09:18 -0800

    Added clearing of form and changing label.

--- src/gypfxml/ui/add_part/AddPartScreenController.java
+++ src/gypfxml/ui/add_part/AddPartScreenController.java
@@ -3,25 +3,49 @@ package gypfxml.ui.add_part;
 import gypfxml.GypFXML;
 import gypfxml.misc.Event;
 import gypfxml.misc.EventManager;
+import gypfxml.model.Part;
 import gypfxml.ui.ScreenResource;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class AddPartScreenController implements Initializable {
     
+    private GypFXML app;
     private boolean inHouse = true;
+    
+    @FXML
+    private RadioButton inHouseRadio;
+    @FXML
+    private Label machineCompanyLab;
+    @FXML
+    private TextField nameInp;
+    @FXML
+    private TextField invInp;
+    @FXML
+    private TextField priceInp;
+    @FXML
+    private TextField maxInp;
+    @FXML
+    private TextField minInp;
+    @FXML
+    private TextField machineCompanyInp;
     
     @FXML
     private void handleInHouseSelect(ActionEvent event) {
         inHouse = true;
+        updateMachineCompanyLab();
     }
     
     @FXML
     private void handleOutsourcedSelect(ActionEvent event) {
         inHouse = false;
+        updateMachineCompanyLab();
     }
     
     @FXML
@@ -34,16 +58,31 @@ public class AddPartScreenController implements Initializable {
         GypFXML.getInstance().showScene(ScreenResource.MAIN);
     }
     
+    private void updateMachineCompanyLab() {
+        machineCompanyLab.setText((inHouse) ? "Machine ID" : "Company Name");
+    }
     
     private void refresh() {
-        //TODO
+        inHouse = true;
+        inHouseRadio.setSelected(true);
+        
+        nameInp.clear();
+        invInp.clear();
+        priceInp.clear();
+        maxInp.clear();
+        minInp.clear();
+        machineCompanyInp.clear();
+        
+        updateMachineCompanyLab();
+        
+        nameInp.requestFocus();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        EventManager eventManager = GypFXML.getInstance().getEventManager();
+        app = GypFXML.getInstance();
         
-        eventManager.on(Event.SCREEN_CHANGED, (Object... data) -> {
+        app.getEventManager().on(Event.SCREEN_CHANGED, (Object... data) -> {
             if (data[0].equals(ScreenResource.ADD_PART)) {
                 refresh();
             }
