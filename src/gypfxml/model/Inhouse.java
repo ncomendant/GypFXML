f9b9effd65a63b35bd199d8d3276de1249658275
ncomendant <nicholas.comendant@gmail.com>
2018-12-06 11:06:56 -0800

    Fixed indentation

--- src/gypfxml/model/Inhouse.java
+++ src/gypfxml/model/Inhouse.java
@@ -2,13 +2,13 @@ package gypfxml.model;
 
 public class Inhouse extends Part {
 	
-	private int machineID;
-	
-	public void setMachineID(int machineID) {
-		this.machineID = machineID;
-	}
-	
-	public int getMachineId() {
-		return machineID;
-	}
+    private int machineID;
+
+    public void setMachineID(int machineID) {
+            this.machineID = machineID;
+    }
+
+    public int getMachineId() {
+            return machineID;
+    }
 }
--- src/gypfxml/model/Outsourced.java
+++ src/gypfxml/model/Outsourced.java
@@ -1,13 +1,13 @@
 package gypfxml.model;
 
 public class Outsourced extends Part {
-	private String companyName;
+    private String companyName;
 	
-	public String getCompanyName() {
-		return companyName;
-	}
-	
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
+    public String getCompanyName() {
+            return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+            this.companyName = companyName;
+    }
 }
